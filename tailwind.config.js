658eea5ba47dd5cc1a54ef9819e0271540c2ee9e
HungH206 <Hoanghungkz4@gmail.com>
2024-12-09 23:30:01 -0600

    Change in Background Color for tailwind.config.js and styles.css, New Image Added, and changes in Index.html, aboutme.html, features.html, students.html.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#9FD89F',
+      },
+    },
   },
   plugins: [],
 }
